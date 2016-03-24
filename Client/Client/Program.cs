51c3ee45873d0afdc2a32149bce73858eb4723bb
Divix <divix55@icloud.com>
2016-03-24 17:16:00 +0100

    Commit all

---
+++ Client/Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nazwa;
+            TcpClient klient = new TcpClient();
+            klient.Connect("127.0.0.1", 1024);
+
+            BinaryWriter writer = new BinaryWriter(klient.GetStream());
+            Console.WriteLine("Witaj! Jak się nazywasz? ");
+            nazwa = Console.ReadLine();
+            Console.WriteLine("Napisz coś do serwera: ");
+            while(true)
+            {
+                writer.Write(nazwa + ": " + Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener serwer = new TcpListener(IPAddress.Parse("127.0.0.1"), 1024);
+
+            serwer.Start();
+            TcpClient newClient = serwer.AcceptTcpClient();
+            TcpClient newClientA = serwer.AcceptTcpClient();
+            BinaryReader reader = new BinaryReader(newClient.GetStream());
+            BinaryReader readerA = new BinaryReader(newClientA.GetStream());
+            Console.WriteLine("Połączenie ustanowione.");
+            while(true)
+            {
+                Console.WriteLine(reader.ReadString());
+                Console.WriteLine(readerA.ReadString());
+            }
+        }
+    }
+}
